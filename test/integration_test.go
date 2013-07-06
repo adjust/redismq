45f986ddf0d459694d68c1576512a79da76ff838
Paul H. Müller <paul@adeven.com>
2013-07-06 23:40:36 +0200

    adds consumer and multi concurrent benchmarks

--- test/integration_test.go
+++ test/integration_test.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	//"fmt"
 	"github.com/adeven/goenv"
 	"github.com/adeven/rqueue"
 	. "github.com/matttproud/gocheck"
 	"math/rand"
+	"runtime"
+	"strconv"
+	"sync"
 	"testing"
 	"time"
 )
@@ -20,6 +24,7 @@ type TestSuite struct {
 var _ = Suite(&TestSuite{})
 
 func (suite *TestSuite) SetUpSuite(c *C) {
+	runtime.GOMAXPROCS(4)
 	rand.Seed(time.Now().UTC().UnixNano())
 	suite.goenv = goenv.NewGoenv("../example/config.yml", "gotesting", "../example/log/test.log")
 	suite.queue = rqueue.NewQueue(suite.goenv, "teststuff")
@@ -217,27 +222,144 @@ func (suite *TestSuite) TestHugePayload(c *C) {
 
 //benchmark single publisher 1k payload
 func (suite *TestSuite) BenchmarkSinglePub1k(c *C) {
-	//1KB payload
 	payload := randomString(1024)
 	for i := 0; i < c.N; i++ {
 		suite.queue.Put(payload)
 	}
 }
 
+//benchmark single consumer 1k payload
+func (suite *TestSuite) BenchmarkSingleCon1k(c *C) {
+	payload := randomString(1024)
+	for i := 0; i < 100000; i++ {
+		suite.queue.Put(payload)
+	}
+	c.ResetTimer()
+	for i := 0; i < c.N; i++ {
+		p, _ := suite.queue.Get(suite.consumer)
+		p.Ack()
+	}
+}
+
 //benchmark single publisher 4k payload
 func (suite *TestSuite) BenchmarkSinglePub4k(c *C) {
-	//1KB payload
 	payload := randomString(1024 * 4)
 	for i := 0; i < c.N; i++ {
 		suite.queue.Put(payload)
 	}
 }
 
+//benchmark single consumer 4k payload
+func (suite *TestSuite) BenchmarkSingleCon4k(c *C) {
+	payload := randomString(1024 * 4)
+	for i := 0; i < 100000; i++ {
+		suite.queue.Put(payload)
+	}
+	c.ResetTimer()
+	for i := 0; i < c.N; i++ {
+		p, _ := suite.queue.Get(suite.consumer)
+		p.Ack()
+	}
+}
+
 //benchmark four publishers 1k payload
+func (suite *TestSuite) BenchmarkFourPub1k(c *C) {
+	//1KB payload
+	var wg sync.WaitGroup
+	payload := randomString(1024)
+	for i := 0; i < c.N; i++ {
+		for j := 0; j < 4; j++ {
+			wg.Add(1)
+			go func() {
+				suite.queue.Put(payload)
+				defer wg.Done()
+			}()
+		}
+		wg.Wait()
+	}
+}
+
+//benchmark four consumers 1k payload
+func (suite *TestSuite) BenchmarkFourCon1k(c *C) {
+	var wg sync.WaitGroup
+	payload := randomString(1024)
+	for i := 0; i < 100000; i++ {
+		suite.queue.Put(payload)
+	}
+	c.ResetTimer()
+	for i := 0; i < c.N; i++ {
+		for j := 0; j < 4; j++ {
+			wg.Add(1)
+			go func(j int) {
+				p, _ := suite.queue.Get(suite.consumer + strconv.Itoa(i) + strconv.Itoa(j))
+				p.Ack()
+				defer wg.Done()
+			}(j)
+		}
+		wg.Wait()
+	}
+}
 
 //benchmark four publisher 4k payload
+func (suite *TestSuite) BenchmarkFourPub4k(c *C) {
+	//1KB payload
+	var wg sync.WaitGroup
+	payload := randomString(1024 * 4)
+	for i := 0; i < c.N; i++ {
+		for j := 0; j < 4; j++ {
+			wg.Add(1)
+			go func() {
+				suite.queue.Put(payload)
+				defer wg.Done()
+			}()
+		}
+		wg.Wait()
+	}
+}
+
+//benchmark four consumers 4k payload
+func (suite *TestSuite) BenchmarkFourCon4k(c *C) {
+	var wg sync.WaitGroup
+	payload := randomString(1024 * 4)
+	for i := 0; i < 100000; i++ {
+		suite.queue.Put(payload)
+	}
+	c.ResetTimer()
+	for i := 0; i < c.N; i++ {
+		for j := 0; j < 4; j++ {
+			wg.Add(1)
+			go func(j int) {
+				p, _ := suite.queue.Get(suite.consumer + strconv.Itoa(i) + strconv.Itoa(j))
+				p.Ack()
+				defer wg.Done()
+			}(j)
+		}
+		wg.Wait()
+	}
+}
 
 //benchmark single publisher and single consumer 1k payload
+func (suite *TestSuite) BenchmarkSingPubSingCon1k(c *C) {
+	//1KB payload
+	var wg sync.WaitGroup
+	payload := randomString(1024)
+	//add first package so consumer doesn't wait for publisher
+	suite.queue.Put(payload)
+	for i := 0; i < c.N; i++ {
+		wg.Add(1)
+		go func() {
+			suite.queue.Put(payload)
+			defer wg.Done()
+		}()
+		wg.Add(1)
+		go func() {
+			p, _ := suite.queue.Get(suite.consumer)
+			p.Ack()
+			defer wg.Done()
+		}()
+		wg.Wait()
+	}
+}
 
 //benchmark single publisher and single consumer 4k payload
 
