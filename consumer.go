fb6584c24578eb5221314ed3bddf235f404900fa
Paul H. Müller <paul@adeven.com>
2013-08-11 17:36:13 +0200

    add unique consumers with heartbeat

--- consumer.go
+++ consumer.go
@@ -1,8 +1,9 @@
 package redismq
 
 import (
-	//"fmt"
+	"fmt"
 	"github.com/adeven/redis"
+	"time"
 )
 
 //Consumers are Watchers that have writing commands
@@ -13,11 +14,28 @@ type Consumer struct {
 func (self *Queue) AddConsumer(name string) (c *Consumer, err error) {
 	c = &Consumer{Broker{Name: name, Queue: self}}
 	//check uniqueness and start heartbeat
-	self.redisClient.SAdd(self.WorkerKey(), name).Val()
-	// if added == 0 {
-	// 	return nil, fmt.Errorf("consumer with this name is already active!")
-	// }
+	added := self.redisClient.SAdd(self.WorkerKey(), name).Val()
+	if added == 0 {
+		val := self.redisClient.Get(c.HeartbeatName()).Val()
+		if val == "ping" {
+			return nil, fmt.Errorf("consumer with this name is already active!")
+		}
+	}
+	c.StartHeartbeat()
 	return c, nil
+}
+
+func (self *Broker) HeartbeatName() string {
+	return self.WorkingName() + "::heartbeat"
+}
+
+func (self *Consumer) StartHeartbeat() {
+	go func() {
+		for {
+			self.Queue.redisClient.SetEx(self.HeartbeatName(), 1, "ping")
+			time.Sleep(500 * time.Millisecond)
+		}
+	}()
 }
 
 func (self *Consumer) parseRedisAnswer(answer *redis.StringReq) (*Package, error) {
