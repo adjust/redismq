69df20a95fd9ba407995a25bddc6582bdccda9a2
adam <adam@straycats.cc>
2017-01-13 17:16:29 +0800

    add Quit function to consumer

---
+++ example/simple_with_cancel.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Straycats/redismq"
+)
+
+func main() {
+	testQueue := redismq.CreateQueue("localhost", "6379", "", 9, "clicks")
+	for i := 0; i < 10; i++ {
+		testQueue.Put("testpayload")
+	}
+	consumer, err := testQueue.AddConsumer("testconsumer")
+	if err != nil {
+		panic(err)
+	}
+	for i := 0; i < 10; i++ {
+		p, err := consumer.Get()
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		fmt.Println(p.CreatedAt)
+		err = p.Ack()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+
+	consumer.Quit()
+	consumer, err = testQueue.AddConsumer("testconsumer")
+	if err != nil {
+		panic(err)
+	}
+}
